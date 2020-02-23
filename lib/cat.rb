38a6dd0b6c760d6cfbb2f89e310f543e0a739c4e
T S <taneymail@gmail.com>
2020-02-23 00:20:42 +0000

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,19 @@
 class Cat
-  # code goes here
+   attr_accessor :owner, :mood
+  attr_reader :name
+  
+  @@all = []
+  
+  def initialize(name,owner)
+    
+    @owner = owner 
+    @name = name
+    @@all << self
+    @mood = "nervous"
+  
+  end 
+  
+  def self.all 
+    @@all 
+  end
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,73 @@
 class Owner
   # code goes here
+  attr_reader :name, :species, :cats 
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name 
+    @cats = cats
+    @species = "human"
+    @@all << self
+  end 
+  
+  def self.all 
+    @@all 
+  end
+  
+  def say_species
+    "I am a human."
+  end
+  
+  def self.count 
+    self.all.count
+  end
+  
+  def self.reset_all
+    self.all.clear
+  end
+  
+  def cats
+    Cat.all.select {|c| c.owner == self}
+  end
+ 
+  def dogs
+    Dog.all.select {|d| d.owner == self}
+  end
+  
+  def buy_cat(name)
+    Cat.new(name,self)
+  end
+  
+  def buy_dog(name)
+    Dog.new(name,self)
+  end
+  
+  def walk_dogs
+   dogs.each {|d| d.mood = "happy"}
+  end
+  
+  def feed_cats
+   cats.each {|c| c.mood = "happy"}
+  end
+  
+  def pets 
+    # pets = []
+    # pets << dogs 
+    # pets << cats
+    # pets.flatten
+    dogs + cats
+  end
+  
+  def sell_pets
+    pets.each do |p|
+      p.mood = "nervous"
+      p.owner = nil 
+    end
+  end
+  
+  def list_pets
+    "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
+  end
+  
 end
